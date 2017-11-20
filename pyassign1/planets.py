9760d906c3086d371b0aaca2575b8acf1f05e938
32805433 <274371065@qq.com>
2017-11-20 17:02:31 +0800

    hw

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,19 +1,17 @@
-import turtle
-import math
-import random
+import turtle, math
 
 def oval(a, b, t):
     c = []
     for j in range(6):
         c.append((a[j]*a[j] - b[j]*b[j]) ** 0.5)
         t[j].penup()
-        t[j].goto(a[j]+c[j], 0)
+        t[j].goto(a[j] + c[j], 0)
         t[j].pendown()
     for i in range(1001):
         for j in range(6):
             co = math.cos(math.pi * i * (9-j) / 500)
             si = math.sin(math.pi * i * (9-j) / 500)
-            r = b[j] * b[j] / (a[j] - c[j] * co)
+            r = b[j] * b[j] / (a[j] - c[j]*co)
             t[j].goto(r * co, r * si)
         
 a = [23.2, 36.3, 50.7, 83.0, 271.9, 502.3]
